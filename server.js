6c8a6301d06c95e9d7c9ac76c9463c65133f5dee
Emerson Mellado <emersonmellado@gmail.com>
2017-08-13 14:24:00 -0700

    Adding mongoDb using mLab

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
 
-app.listen(3000, function(){
-  console.log('listening on 3000');
-})
+var db
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.use(bodyParser.urlencoded({extended: true}))
+
+MongoClient.connect('mongodb://crud-ops:crud-ops@ds137360.mlab.com:37360/crud-ops', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, function(){
+    console.log('listening on 3000')
+  })
+
+  app.get('/', function(req, res){
+    //res.sendFile(__dirname + '/index.html')
+    var cursor = db.collection('quotes').find().toArray(function(err, results){
+      console.log(results)
+    })
+  })
+
+  app.post('/quotes', (req, res) =>{
+    db.collection('quotes').save(req.body, (err, result) => {
+      if (err) return console.log(err)
+
+      console.log('saved to database')
+      res.redirect('/')
+    })
+  })
 })
