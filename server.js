4e607d4afa72ed29834811807bc441c4d0ee8dca
Emerson <emersonmellado@gmail.com>
2017-09-09 16:54:02 -0700

    Adding ejs

--- server.js
+++ server.js
@@ -6,26 +6,28 @@ const MongoClient = require('mongodb').MongoClient
 var db
 
 app.use(bodyParser.urlencoded({extended: true}))
+app.set('view engine', 'ejs')
 
 MongoClient.connect('mongodb://crud-ops:crud-ops@ds137360.mlab.com:37360/crud-ops', (err, database) => {
   if (err) return console.log(err)
-  db = database
+    db = database
   app.listen(3000, function(){
     console.log('listening on 3000')
   })
 
-  app.get('/', function(req, res){
-    //res.sendFile(__dirname + '/index.html')
-    var cursor = db.collection('quotes').find().toArray(function(err, results){
-      console.log(results)
-    })
+  app.get('/', (req, res) => {
+    db.collection('quotes').find().toArray((err, result) => {
+      if (err) return console.log(err)
+      // renders index.ejs
+    res.render('index.ejs', {quotes: result})
+  })
   })
 
   app.post('/quotes', (req, res) =>{
     db.collection('quotes').save(req.body, (err, result) => {
       if (err) return console.log(err)
 
-      console.log('saved to database')
+        console.log('saved to database')
       res.redirect('/')
     })
   })
